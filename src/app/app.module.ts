3cff36e61c246e27685e8ce42af477cbba00de17
Ambinintsoa <rahmbints@gmail.com>
2024-04-24 17:33:08 +0300

    #61 Creating and using Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './header/admin/admin.component';
+import { HomeComponent } from './header/home/home.component';
+import { HeroComponent } from './header/home/hero/hero.component';
+import { SidebarComponent } from './header/home/sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
+import { UserDetailComponent } from './header/admin/user-detail/user-detail.component';
+import { UserListComponent } from './header/admin/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AdminComponent,
+    HomeComponent,
+    HeroComponent,
+    SidebarComponent,
+    UserDetailComponent,
+    UserListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
